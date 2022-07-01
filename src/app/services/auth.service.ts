9c048e74d5719c8ddc3958c3d29566dfc1da1f12
anmLargus <andresnm29@gmail.com>
2022-07-01 13:04:14 -0300

    auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { map, Observable } from 'rxjs';
+
+const LOGIN_ENDPOINT = "http://localhost:8081/login";
+
+const TOKEN_KEY = 'token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+
+
+  constructor( private http: HttpClient ) { }
+
+  login(usuario: string, password: string): Observable<boolean> {
+    return this.http.post( LOGIN_ENDPOINT, {usuario , password})
+      .pipe(map( (res: any) => {
+        try {
+          if(res?.token) {
+            localStorage.setItem(TOKEN_KEY, res.token);
+          } 
+          return true;
+        } catch (error) {
+          return false;
+        }
+      }))
+  }
+
+  logout(): void {
+    localStorage.removeItem(TOKEN_KEY);
+  }
+
+  isLogged(): boolean {
+    // Debe chequearse que exista el token en el localStorage y tambien chequear claims
+    return localStorage.getItem(TOKEN_KEY) !== null;
+  }
+  
+}
